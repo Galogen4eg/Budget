6e6197410a2b5aa894fb3d6abb409b9db12bdf58
Galogen4eg <15gena1356@gmail.com>
2025-12-10 00:10:29 +0300

    Add files via upload

---
+++ core.js
@@ -0,0 +1,37 @@
+window.data = window.data || {
+    users: [],
+    roomPassword: null,
+    shopping: {
+        items: [],
+        templates: [],
+        telegram: { token: '', chatId: '' }
+    }
+};
+
+window.firebaseConfig = window.firebaseConfig || {
+    databaseURL: 'https://budg-1d5e0-default-rtdb.europe-west1.firebasedatabase.app'
+};
+
+window.db = window.db || null;
+(function initFirebaseCore() {
+    try {
+        if (typeof firebase !== 'undefined') {
+            if (firebase.apps.length === 0) firebase.initializeApp(window.firebaseConfig);
+            window.db = firebase.database();
+
+            if (firebase.auth) {
+                firebase.auth().signInAnonymously().catch((error) => {
+                    console.error('Firebase auth error:', error);
+                });
+            }
+        }
+    } catch (e) {
+        console.error('Firebase init error:', e);
+    }
+})();
+
+window.syncData = function syncData() {
+    if (window.db && window.roomsManager?.currentRoomId && window.roomsManager.dataRef) {
+        window.roomsManager.dataRef.update({ data: window.data, lastUpdated: firebase.database.ServerValue.TIMESTAMP });
+    }
+};

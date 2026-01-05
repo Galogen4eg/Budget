bcc02479efe9806eeee8c7ec7a26b5165e8d6e63
Galogen4eg <15gena1356@gmail.com>
2026-01-05 15:37:54 +0300

    Delete components directory

--- components/FinancialWeatherWidget.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-// Виджет полностью удален по запросу пользователя.
-const FinancialWeatherWidget: React.FC<any> = () => {
-  return null;
-};
-
-export default FinancialWeatherWidget;

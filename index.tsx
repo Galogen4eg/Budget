e6ee7f41ac538dae961f72278f41946f49b4909c
Galogen4eg <15gena1356@gmail.com>
2026-01-09 16:44:34 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -4,25 +4,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { DataProvider } from './contexts/DataContext';
-// @ts-ignore
-import { registerSW } from 'virtual:pwa-register';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
-// Автоматическая регистрация и обновление Service Worker
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm('Доступно обновление. Перезагрузить?')) {
-      updateSW(true);
-    }
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline');
-  },
-});
 
 const root = createRoot(rootElement);
 root.render(

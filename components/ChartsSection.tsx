d2e3706ce4ff1a76a5bc96a52fea6312637254bb
Galogen4eg <15gena1356@gmail.com>
2026-09-23 23:58:30 +0300

    Delete

--- components/ChartsSection.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-
-// Виджет "Структура" удален.
-const ChartsSection: React.FC<any> = () => {
-  return null;
-};
-
-export default ChartsSection;
--- service-worker.js
+++
@@ -1,2 +0,0 @@
-// This file is now managed by vite-plugin-pwa. 
-// Do not add manual logic here unless you configure 'injectManifest' strategy in vite.config.ts
